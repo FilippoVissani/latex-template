2d0611ce43f2dde4fc4dea5bad57b5a597b6f9de
paolo <paolo.penazzi@studio.unibo.it>
2023-04-04 14:41:05 +0200

    feat: add semantic release

---
+++ release.config.js
@@ -0,0 +1,19 @@
+const config = require('semantic-release-preconfigured-conventional-commits')
+
+const releaseBranches = ["main"]
+config.branches = releaseBranches
+config.plugins.push(
+    ["@semantic-release/exec", {
+    }],
+    ["@semantic-release/github", {
+        "assets": [
+            // Change this path if your pdf has another name.
+            { "path": "thesis-main.pdf" },
+        ]
+    }],
+    ["@semantic-release/git", {
+        "assets": ["CHANGELOG.md", "package.json"],
+        "message": "chore(release)!: [skip ci] ${nextRelease.version} released"
+    }],
+)
+module.exports = config
